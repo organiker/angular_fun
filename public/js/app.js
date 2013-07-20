c82749e37775e379ec83c5c73bdcac8bf50aec56
Jiyeon Woo <jiyeon8497@gmail.com>
2013-07-20 18:30:48 -0400

    filtering

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,8 @@
 function MyCtrl($scope) {
-  $scope.myVariable = "hello MIT";
-  $scope.showHidden = false;
-
   $scope.items = [
     {name: "Banana", rating: 3},
     {name: "Vanilla", rating: 5},
     {name: "Strawberry", rating: 7},
     {name: "Rum Raisin", rating: 2}
   ];
-
-  $scope.doMagic = function() {
-    console.log("test!");
-    $scope.myVariable = "BOO!";
-    $scope.showHidden = true;
-  }
 }
