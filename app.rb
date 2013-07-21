4ec99d8ff966e15690e83ed6b7648ac2a5c647eb
Jiyeon Woo <jiyeon8497@gmail.com>
2013-07-21 12:11:22 -0400

    add yaml & fix validation

--- app.rb
+++ app.rb
@@ -1,7 +1,15 @@
 require 'sinatra'
+require 'yaml'
+require 'json'
 
 set :public_dir, 'public'
 
-get '/' do
-  "hello world"
+get '/items.json' do
+	file = File.read('db.yml')
+	db = YAML::load(file)
+	db.to_json
+end
+
+post '/items' do
+  puts "Received POST to items: #{request.body.read}"
 end
--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,16 @@
-function MyCtrl($scope) {
-  $scope.items = [
-    {name: "Banana", rating: 3},
-    {name: "Vanilla", rating: 5},
-    {name: "Strawberry", rating: 7},
-    {name: "Rum Raisin", rating: 2}
-  ];
+function MyCtrl($scope, $http) {
+  
+  $http.get("/items.json").success(function(data) {
+    // console.log(data);
+    $scope.items = data;
+  });
+
+  $scope.addItem = function() {
+    $scope.items.push({name: $scope.newItem, rating: $scope.newRating});
+    // push new data back to backend
+    $http.post("/items", {name: $scope.newItem, rating: $scope.newRating}).
+      success(function(d) { 
+        console.log("success");
+      });
+  }
 }
